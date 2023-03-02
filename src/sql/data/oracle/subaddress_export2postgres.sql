90fcca3c8d695a5ed3c78cc8da5cde3a73ca206e
Matt Schell <mschell@doitt.nyc.gov>
2023-03-02 16:18:03 -0500

    adds subaddress out of band replication

---
+++ src/sql/data/oracle/subaddress_export2postgres.sql
@@ -0,0 +1,40 @@
+select 'insert into subaddress values ('
+     || sub_address_id
+     || ',''' || melissa_suite || ''''
+     || ',' || ap_id
+     || ',''' || additional_loc_info || ''''
+     || ',''' || building || ''''
+     || ',''' || floor || ''''
+     || ',''' || unit || ''''
+     || ',''' || room || ''''
+     || ',''' || seat || ''''
+     || ',''' || created_by || ''''
+     || case  
+            when created_date is not null 
+            then 
+                ',''' || to_char(created_date) || ''''
+            else ',null::date'
+       end
+     || ',''' || modified_by || ''''
+     || case  
+            when modified_date is not null 
+            then 
+                ',''' || to_char(modified_date) || ''''
+            else ',null::date'
+       end
+     || ',' || NVL(boroughcode,'null')
+     || case  
+            when validation_date is not null 
+            then 
+                ',''' || to_char(validation_date) || ''''
+            else ',null::date'
+       end
+     || ',''' || update_source || ''''
+     || ',''' || usps_hnum || ''''
+     || ',' || objectid
+     || ',''' || globalid || ''''
+     || ');'
+from 
+    cscl.subaddress_evw;
+
+   
---
+++ src/sql/definition/postgresql/subaddress.sql
@@ -0,0 +1,27 @@
+-- the source cscl database is completely unconstrained
+-- it relies on application code to enforce data integrity
+-- we will engage in some basic best practices here
+drop table subaddress if exists;
+create table subaddress (
+	sub_address_id          int4 constraint subaddress_pkc primary key,
+	melissa_suite           varchar(50) not null,
+	ap_id                   int4 not null,
+	additional_loc_info     varchar(50) null,
+	building                varchar(50) null,
+	floor                   varchar(50) null,
+	unit                    varchar(50) null,
+	room                    varchar(50) null,
+	seat                    varchar(50) null,
+	created_by              varchar(50) null,
+	created_date            date null,
+	modified_by             varchar(50) null,
+	modified_date           date null,
+	boroughcode             int4 null,
+	validation_date         varchar(50) null,
+	update_source           varchar(50) null,
+	usps_hnum               varchar(50) null,
+	objectid                int4 not null,
+	globalid                varchar(38) not null default '{00000000-0000-0000-0000-000000000000}'::character varying,
+    constraint subaddress_objectid_uqc unique(objectid)
+);
+grant select on subaddress to "pscscl";
