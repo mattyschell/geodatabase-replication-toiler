9fdb8687fb11a0e0572a2834eee0f4669d0368ee
Matt Schell <mschell@doitt.nyc.gov>
2023-04-10 11:31:21 -0400

    adds replica checker resolves #17

---
+++ checkreplica.py
@@ -0,0 +1,45 @@
+import sys
+import os
+import logging
+import time 
+
+import punkreplica
+
+
+if __name__ == "__main__":
+
+    pparent      = sys.argv[1] # C:\temp\cscl.gdb
+    pchild       = sys.argv[2] # D:\temp\cscl.gdb
+    preplicaname = sys.argv[3] # punk
+    playerlist   = sys.argv[4] # nybb
+
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    targetlog = os.path.join(os.environ['TARGETLOGDIR'] 
+                            ,'checkreplica-{0}-{1}.log'.format(preplicaname, timestr))
+
+    logging.basicConfig(filename=targetlog
+                       ,level=logging.INFO)
+
+    checkreplica = punkreplica.Replica(pparent
+                                      ,pchild
+                                      ,preplicaname)
+
+    result = 'pass'
+
+    logging.info('| featurelayer | rowcountdifference |')
+    logging.info('| --- | --- |')
+
+    for featurelayer in playerlist.split(','):
+        
+        kount = checkreplica.compare(featurelayer)
+
+        if kount > 0:
+            result = 'fail'
+
+        logging.info('| {0} | {1} | '.format(featurelayer,kount))
+
+
+    if result == 'pass':
+        exit(0)
+    else:
+        exit(1)
--- geodatabase-scripts/sample-create-punk-replica.bat
+++ geodatabase-scripts/sample-create-punk-replica.bat
@@ -15,14 +15,23 @@ set PYTHONPATH=%REPLICATOILER%\src\py
 set PROPY=c:\Progra~1\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe
 set TARGETLOGDIR=%GISDIR%\geodatabase-scripts\logs\create-punk-replica\
 set BATLOG=%TARGETLOGDIR%create-punk-replica.log
-REM failures send logs\create-punk-replica\create-punk-replica.bat
-REM success with checks send the most recent check log
+REM failures send batch logs\create-punk-replica\create-punk-replica.bat
+REM success with QA send the most recent checkreplica-replicaname-*.log
 echo starting up our work on %REPLICANAME% on %date% at %time% > %BATLOG%
 %PROPY% %GISDIR%\geodatabase-replication-toiler\createpunkreplica.py %SDEPARENT% %SDECHILD% %REPLICANAME% && (
   echo. >> %BATLOG% && echo replicated %SDEPARENT% to %SDECHILD% on %date% at %time% >> %BATLOG%
 ) || (
   %PROPY% %REPLICATOILER%\notify.py ": Failed to replicate %SDEPARENT% to %SDECHILD%" %NOTIFY% "create-punk-replica" && EXIT /B 1
 )  
+echo. >> %BATLOG% && echo checking %SDEPARENT% comparing to %SDECHILD% on %date% at %time% >> %BATLOG%
+%PROPY% %GISDIR%\geodatabase-replication-toiler\checkreplica.py %SDEPARENT% %SDECHILD% %REPLICANAME% %REPLICACHECKLAYERS% && (
+  echo. >> %BATLOG% && echo passed check %SDEPARENT% to %SDECHILD% on %date% at %time% >> %BATLOG%
+) || (
+  %PROPY% %REPLICATOILER%\notify.py ": Failed to pass check %SDEPARENT% to %SDECHILD%" %NOTIFY% "create-punk-replica" && EXIT /B 1
+)  
 echo. >> %BATLOG% && echo checking %REPLICASOURCE% comparing to %SDECHILD% on %date% at %time% >> %BATLOG%
-REM check replica goes here
-%PROPY% %REPLICATOILER%\notify.py ": Successfully replicated %SDEPARENT% to %SDECHILD%" %NOTIFY% "create-punk-replica"
+%PROPY% %GISDIR%\geodatabase-replication-toiler\checkreplica.py %REPLICASOURCE% %SDECHILD% %REPLICANAME% %REPLICACHECKLAYERS% && (
+  %PROPY% %REPLICATOILER%\notify.py ": passed comparison of %REPLICASOURCE% to %SDECHILD%" %NOTIFY% "checkreplica-%REPLICANAME%" && EXIT /B 1
+) || (
+  %PROPY% %REPLICATOILER%\notify.py ": QA comparison of %REPLICASOURCE% to %SDECHILD%" %NOTIFY% "checkreplica-%REPLICANAME%" && EXIT /B 1
+)  
